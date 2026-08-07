479c74af76a2f06451709cb0430fe28ae1008709
Julius Evan C. Estonilo <116896327+7equinox@users.noreply.github.com>
2026-08-07 13:43:09 +0800

    Make func operate to call MDAS

--- script.js
+++ script.js
@@ -17,3 +17,19 @@ function multiply(multiplicand, multiplier) {
 function divide(dividend, divisor) {
     return dividend / divisor;
 }
+
+function operate(num1, operator, num2) {
+    switch(operator) {
+        case '+':
+            add(num1, num2);
+            break;
+        case '-':
+            subtract(num1, num2);
+            break;
+        case '*':
+            multiply(num1, num2);
+            break;
+        case '/':
+            divide(num1, num2);
+    }
+}
