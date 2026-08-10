d5a0d634038d02e824a3ed07e7c02592f855ca99
Julius Evan C. Estonilo <116896327+7equinox@users.noreply.github.com>
2026-08-10 16:38:30 +0800

    Add keyboard support

--- script.js
+++ script.js
@@ -88,121 +88,145 @@ function storeValues() {
     return getFinalValue(num1, operator, num2);
 }
 
+function setCalcInput(event) {
+    let charInput = '';
+
+    if(event.type === 'click') {
+        charInput = event.target.textContent;
+    } else {
+        charInput = event.key;
+    }
+
+    // Avoid NaN immediately
+    if(objResult.textContent === 'undefined') {
+        objResult.textContent = '';
+    }
+
+    switch(charInput) {
+        // Clear button
+        case 'C':
+        case 'c':
+        case 'Delete':
+            objResult.textContent = '';
+            boolHasOp = false;
+            boolHasDeci = false;
+            break;
+        // Backspace button
+        case '⌫':
+        case 'Backspace':
+            // Clear if operation complete
+            if(boolHasCompleteOp) {
+                objResult.textContent = '';
+                break;
+            }
+            // Remove space if last input is op
+            if(/^[*/+-]?$/.test(objResult.textContent.at(-2))) {
+                objResult.textContent =
+                    objResult.textContent
+                    .replaceAll(' ', '');
+            }
+            objResult.textContent =
+                objResult.textContent.slice(0, -1);
+            break;
+        case '=':
+        case 'Enter':
+            // Invalid 1st or 2nd input num '.'
+            if(objResult.textContent.at(-1) === '.') {
+                break;
+            }
+
+            // Invalid unary op
+            if(/^[+-]?$/.test(objResult.textContent)) {
+                break;
+            }
+
+            // Invalid inputs if 1st num and op only
+            if(/^[*/+-]?$/.test(objResult.textContent.at(-2))) {
+                break;
+            }
+
+            // Valid input/s if 1st num only OR 1st num, op, and 2nd num
+            if(objResult.textContent !== '') {
+                objResult.textContent = storeValues();
+                
+                // Eligible to add a decimal for integer output
+                if(Number.isInteger(Number(objResult.textContent))) {
+                    boolHasDeci = false;
+                }
+
+                // Signal for add operation or start new calc after result
+                if(boolHasOp) {
+                    boolHasOp = false;
+                    boolHasCompleteOp = true;
+                }
+            }
+            break;
+        case '+':
+        case '-':
+            // Unary operator of a num
+            if(/^[+-]?$/.test(objResult.textContent)) {
+                objResult.textContent = charInput;
+                break;
+            }
+            
+        case '*':
+        case '/':
+            // Invalid 1st input num '.'
+            if(objResult.textContent.at(-1) === '.') {
+                break;
+            }
+
+            // Invalid if unary only before op
+            if(/^[+-]?$/.test(objResult.textContent)) {
+                break;
+            }
+
+            // Evaluate initial pair of nums
+            if(boolHasOp) {
+                objResult.textContent = storeValues();
+                boolHasOp = false;
+            }
+
+            objResult.textContent += ` ${charInput} `;
+            boolHasOp = true;
+            boolHasDeci = false;
+            break;
+        case '.':
+            // One decimal is allowed per input num
+            if(boolHasDeci) {
+                break;
+            }
+            boolHasDeci = true;
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            // Start new calculation after result
+            if(boolHasCompleteOp && !boolHasOp) {
+                boolHasCompleteOp = false;
+                objResult.textContent = '';
+            }
+            objResult.textContent += charInput;
+    }
+
+    // One decimal for float num
+    if(boolHasDeci) {
+        objDeciBtn.disabled = true;
+    } else {
+        objDeciBtn.disabled = false;
+    }
+}
+
 // Listen what user clicks
 objBtns.forEach(objBtn => {
-    objBtn.addEventListener('click', (event) => {
-        const charInput = objBtn.textContent;
-
-        // Avoid NaN immediately
-        if(objResult.textContent === 'undefined') {
-            objResult.textContent = '';
-        }
-
-        switch(charInput) {
-            // Clear button
-            case 'C':
-                objResult.textContent = '';
-                boolHasOp = false;
-                boolHasDeci = false;
-                break;
-            // Backspace button
-            case '⌫':
-                // Clear if operation complete
-                if(boolHasCompleteOp) {
-                    objResult.textContent = '';
-                    break;
-                }
-                // Remove space if last input is op
-                if(/^[*/+-]?$/.test(objResult.textContent.at(-2))) {
-                    objResult.textContent =
-                        objResult.textContent
-                        .replaceAll(' ', '');
-                }
-                objResult.textContent =
-                    objResult.textContent.slice(0, -1);
-                break;
-            case '=':
-                // Invalid 1st or 2nd input num '.'
-                if(objResult.textContent.at(-1) === '.') {
-                    break;
-                }
-
-                // Invalid unary op
-                if(/^[+-]?$/.test(objResult.textContent)) {
-                    break;
-                }
-
-                // Invalid inputs if 1st num and op only
-                if(/^[*/+-]?$/.test(objResult.textContent.at(-2))) {
-                    break;
-                }
-
-                // Valid input/s if 1st num only OR 1st num, op, and 2nd num
-                if(objResult.textContent !== '') {
-                    objResult.textContent = storeValues();
-                    
-                    // Eligible to add a decimal for integer output
-                    if(Number.isInteger(Number(objResult.textContent))) {
-                        boolHasDeci = false;
-                    }
-
-                    // Signal for add operation or start new calc after result
-                    if(boolHasOp) {
-                        boolHasOp = false;
-                        boolHasCompleteOp = true;
-                    }
-                }
-                break;
-            case '+':
-            case '-':
-                // Unary operator of a num
-                if(/^[+-]?$/.test(objResult.textContent)) {
-                    objResult.textContent = charInput;
-                    break;
-                }
-                
-            case '*':
-            case '/':
-                // Invalid 1st input num '.'
-                if(objResult.textContent.at(-1) === '.') {
-                    break;
-                }
-
-                // Invalid if unary only before op
-                if(/^[+-]?$/.test(objResult.textContent)) {
-                    break;
-                }
-
-                // Evaluate initial pair of nums
-                if(boolHasOp) {
-                    objResult.textContent = storeValues();
-                    boolHasOp = false;
-                }
-
-                objResult.textContent += ` ${charInput} `;
-                boolHasOp = true;
-                boolHasDeci = false;
-                break;
-            case '.':
-                // One decimal is allowed per input num
-                if(boolHasDeci) {
-                    break;
-                }
-                boolHasDeci = true;
-            default:
-                // Start new calculation after result
-                if(boolHasCompleteOp && !boolHasOp) {
-                    boolHasCompleteOp = false;
-                    objResult.textContent = '';
-                }
-                objResult.textContent += charInput;
-        }
-
-        // One decimal for float num
-        if(boolHasDeci) {
-            objDeciBtn.disabled = true;
-        } else {
-            objDeciBtn.disabled = false;
-        }
-    });
+    objBtn.addEventListener('click', setCalcInput);
 });
+
+// Keyboard support
+document.addEventListener('keydown', setCalcInput);
